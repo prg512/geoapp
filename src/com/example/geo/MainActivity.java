b2cca3845e71aa747726632ab7ec2edace5d9556
Phil Geiger <araevin13@gmail.com>
2014-10-15 22:13:15 -0500

    first commitq

---
+++ src/com/example/geo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.geo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		WebView myWebView = (WebView) findViewById(R.id.webView1);
+		WebSettings webSettings = myWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		myWebView.loadUrl("https://google-developers.appspot.com/maps/documentation/javascript/examples/full/geocoding-simple");
+	}
+}
